66e3a25da92aecb273919a638c41faac28507040
Amir <itzdele1372@gmail.com>
2025-04-21 08:31:27 +0330

    added django-filter for sorting ,viewsets for trip and url paths with router

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from .views import *
+from .views import Tripsview
 
+router = DefaultRouter()
+router.register("trips", Tripsview)
 urlpatterns = [
-    path('basic_model/', BasicModelView.as_view(), name="basic_model"),
-    path('basic_model/<int:pk>', BasicModelView.as_view(), name="basic_model"),
+    path("", include(router.urls)),
+    path("basic_model/", BasicModelView.as_view(), name="basic_model"),
+    path("basic_model/<int:pk>", BasicModelView.as_view(), name="basic_model"),
 ]
