c44c4f5e518e16f4a8046e0f3117379b27d2ffb8
Amir <itzdele1372@gmail.com>
2025-04-23 15:58:32 +0330

    hotfix

--- backend/api/models/Tripserializers.py
+++ backend/api/models/Tripserializers.py
@@ -3,7 +3,7 @@ from .TripsModel import Trip
 from rest_framework import serializers
 
 
-class Tripserializer(serializers.Serializer):
+class Tripserializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
         fields = "__all__"
--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -1,3 +1,5 @@
 from .BasicModel import BasicModel, BasicModelSerializer
 from .Hotel import Hotel, HotelSerializer
 from .Room import Room, RoomSerializer
+from .Tripserializers import Tripserializer
+from .TripsModel import Trip
