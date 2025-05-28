ca8b3e1f10f9bc6afcd07104ab4bacabc9209f86
ixor911 <igorbutenko9111@gmail.com>
2025-05-28 16:47:22 +0200

    Hotel price filter

--- backend/api/views/HotelView.py
+++ backend/api/views/HotelView.py
@@ -1,3 +1,5 @@
+from enum import unique
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters, decorators, response, exceptions
 
@@ -41,3 +43,22 @@ class HotelView(viewsets.ModelViewSet):
             object.delete()
 
         return response.Response("Data was wiped", status=exceptions.status.HTTP_200_OK)
+
+    @decorators.action(detail=False, methods=['get'])
+    def price(self, request, *args, **kwargs):
+        from ..models import Room
+
+        max_price = request.query_params.get('max')
+        min_price = request.query_params.get('min')
+
+        rooms = Room.objects.all()
+        rooms = rooms.filter(price__lte=max_price) if max_price else rooms
+        rooms = rooms.filter(price__gte=min_price) if min_price else rooms
+
+        hotels = [room.hotel for room in rooms]
+        hotels = list(set(hotels))
+
+        return response.Response(
+            HotelSerializer(hotels, many=True).data,
+            status=exceptions.status.HTTP_200_OK
+        )
