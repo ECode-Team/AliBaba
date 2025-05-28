71d9ddde52a18ff65182b099995a82cf403e63f0
ixor911 <igorbutenko9111@gmail.com>
2025-05-28 14:49:18 +0200

    RoomBook checkup (Not tested)

--- backend/api/models/RoomBook.py
+++ backend/api/models/RoomBook.py
@@ -1,6 +1,5 @@
 from django.db import models
-from rest_framework import serializers
-import datetime
+from rest_framework import serializers, exceptions
 from . import User, Room
 
 
@@ -20,15 +19,30 @@ class RoomBookSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+    def check_room(self, room_id, begin, end):
+        books : list[RoomBook] = RoomBook.objects.filter(room=room_id)
+
+        for book in books:
+            if book.begin < begin < book.end or  book.begin < end < book.end:
+                return False
+
+        return True
+
     def create(self, validated_data):
+        if not self.check_room(
+                room_id=validated_data.get('room'),
+                begin=validated_data.get('begin'),
+                end=validated_data.get('end')
+        ):
+            raise exceptions.NotAcceptable()
+
         duration = validated_data.get('end') - validated_data.get('begin')
 
-        room_book = RoomBook.objects.create(
+        return super().create({
             **validated_data,
-            duration = duration.days
-        )
+            duration: duration.days
+        })
 
-        return room_book
 
 
 
