48069edcf32296d08ba2b934968b100f8eb5c15e
AmirabbasRouintan <amirabbas.helma8693@gmail.com>
2025-07-06 13:32:10 +0330

    DetailPage.v2

--- frontend/vite-project/backend/api/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .BasicModel import BasicModel, BasicModelSerializer
-from .Hotel import Hotel, HotelSerializer
-from .Room import Room, RoomSerializer
-from .Trip import Trip, TripSerializer
-from .User import User, UserSerializer
-from .TripBook import TripBook, TripBookSerializer
-from .RoomBook import RoomBook, RoomBookSerializer
-from .Review import Review, ReviewSerializer
