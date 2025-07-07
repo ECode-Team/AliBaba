5a6946cd86ce1a7c2c485e1d477e100408c377e8
yasin0121 <ecopy369@gmail.com>
2025-07-07 18:56:17 +0330

    search v.1

--- frontend/vite-project/src/Components/mainpage/search.jsx
+++ frontend/vite-project/src/Components/mainpage/search.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
-import "../../styles/mainpage/background.scss";
+import "../../styles/mainpage/search.scss";
+//Backgrounds
 import DomesticFlight from "../../assets/images/domesticflight-light.webp";
 import ForeignFlight from "../../assets/images/foreignflight-light.webp";
 import Train from "../../assets/images/train-light.webp";
@@ -9,6 +10,14 @@ import Tour from "../../assets/images/tour-light.webp";
 import Villa from "../../assets/images/villa-light.webp";
 import Visa from "../../assets/images/visa-light.webp";
 import TravelInsurance from "../../assets/images/insurance-light.webp";
+//Icons
+import innerFlight from "../../assets/icons/innerflight.svg";
+import outerFlight from "../../assets/icons/foreignflight.svg";
+import trainIcon from "../../assets/icons/train.svg";
+import busIcon from "../../assets/icons/bus.svg";
+import hotelIcon from "../../assets/icons/hotel.svg";
+import tourIcon from "../../assets/icons/tour.svg";
+import villaIcon from "../../assets/icons/villa.svg";
 
 export const Search = () => {
     const { mode } = useParams();
@@ -26,8 +35,23 @@ export const Search = () => {
     }
 
     return (
-        <div className="background-container">
-            <div className="background-image" style={{ backgroundImage: `url(${imageMap[mode] || DomesticFlight})` }}>
+        <div className="container">
+            <div className="background-container">
+                <div className="background-image" style={{ backgroundImage: `url(${imageMap[mode] || DomesticFlight})` }}></div>
+            </div>
+            <div className="search-container">
+                <div className="search-top">
+                    <ul className="search-top-items">
+                        <li><img src={innerFlight} /><span>Domestic flight</span></li>
+                        <li><img src={outerFlight} /><span>Foreign flight</span></li>
+                        <li><img src={trainIcon} /><span>train</span></li>
+                        <li><img src={busIcon} /><span>bus</span></li>
+                        <li><img src={hotelIcon} /><span>hotel</span></li>
+                        <li><img src={tourIcon} /><span>tour</span></li>
+                        <li><img src={villaIcon} /><span>Villa & residence</span></li>
+                    </ul>
+                </div>
+                <div className="search-bottom"></div>
             </div>
         </div>
     )
