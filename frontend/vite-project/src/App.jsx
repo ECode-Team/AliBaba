c0f9bf5825996155c2dbe10bdf58c8a7dbf7b5dc
parham0121 <parysolh@gmail.com>
2025-07-13 09:50:15 +0330

    merge develop

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -10,11 +10,10 @@ import Detail from "./Pages/detailpage";
 import { Homepage } from "./Pages/homepage";
 import { Payment } from "./Pages/paymentpage";
 // import { Userpage } from "./Pages/userpage";
-import { Train } from "./Pages/bookingpage/train/train";
+import { Train } from "./Pages/bookingpage/train";
 import { FetchAPI } from "./Components/fetch";
 import { useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
-import { Accordion } from "./Components/mainpage/accordion";
 import "../src/styles/general.scss";
 
 
@@ -28,8 +27,8 @@ const AnimatedRoutes = () => {
         <Route path="/Booking/bus" element={<Bus />} />
         <Route path="/Booking/train" element={<Train />} />
         <Route path="/Booking/hotel" element={<Hotel />} />
-        <Route path="/Booking/flights" element={<Flights />} />
-        <Route path="/Booking/international" element={<International />} />
+        <Route path="/Booking/Innerflight" element={<Innerflight />} />
+        <Route path="/Booking/Foreignflight" element={<Foreignflight />} />
         <Route path="/Booking/vila" element={<Vila />} />
         <Route path="/DetailPage" element={<Detail />} />
         <Route path="/Payment" element={<Payment />} />
