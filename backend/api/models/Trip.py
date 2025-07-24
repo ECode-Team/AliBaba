d767a83fd6d9b0cb49dbe28a659851bcef0266f6
ixor911 <igorbutenko9111@gmail.com>
2025-07-24 13:25:41 +0200

    Trip -country fields -country / city validation -airports validation

--- backend/api/models/Trip.py
+++ backend/api/models/Trip.py
@@ -1,10 +1,13 @@
 from django.db import models
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
+from .. import geo
+
 
 TRANSFER_TYPES = [
     ("Train", "Train"),
     ("Bus", "Bus"),
-    ("Flight", "Flight")
+    ("Flight", "Flight"),
+    ("Domestic Flight", "Domestic Flight")
 ]
 
 PASSENGERS_TYPES = [
@@ -13,24 +16,19 @@ PASSENGERS_TYPES = [
     ("First class", "First")
 ]
 
-CITIES = [
-    ("Tehran", "Tehran"),
-    ("Shiraz", "Shiraz"),
-    ("Mashhad", "Mashhad"),
-    ("Gheshm", "Gheshm"),
-    ("London", "London"),
-    ("Dortmund", "Dortmund"),
-    ("Gamburg", "Gamburg"),
-]
-
-
 class Trip(models.Model):
     transfer_type = models.CharField(choices=TRANSFER_TYPES, max_length=30)
     passengers_type = models.CharField(choices=PASSENGERS_TYPES, max_length=30)
     passengers_max = models.PositiveIntegerField(blank=True, null=True)
 
-    depart = models.CharField(choices=CITIES, max_length=30)
-    arrive = models.CharField(choices=CITIES,max_length=30)
+    depart = models.CharField(choices=[(item, item) for item in geo.geos_cities], max_length=30)
+    depart_country = models.CharField(choices=[(item, item) for item in geo.geos.keys()], max_length=30)
+
+    arrive = models.CharField(choices=[(item, item) for item in geo.geos_cities], max_length=30)
+    arrive_country = models.CharField(choices=[(item, item) for item in geo.geos.keys()], max_length=30)
+
+    logo = models.CharField(max_length=30)
+
     depart_time = models.TimeField()
     arrive_time = models.TimeField()
 
@@ -41,3 +39,46 @@ class TripSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
         fields = '__all__'
+
+    @classmethod
+    def validate_city(cls, data):
+        """
+        Validate city in country
+        """
+
+        if data.get('depart') not in geo.geos.get(data.get('depart_country')) or \
+                data.get('arrive') not in geo.geos.get(data.get('arrive_country')):
+            raise exceptions.ParseError('City and Country error')
+
+    @classmethod
+    def validate_airport(cls, data):
+        """
+        Validate Domestic Flight can only be Iran
+        Validate City should have airport
+        """
+
+        if data.get('transfer_type') == "Domestic Flight" and \
+                "Iran" in (data.get('depart_country'), data.get('arrive_country')):
+            raise exceptions.ParseError('Domestic Flight has different country destination')
+
+        if data.get('transfer_type') == "Flight" and \
+                (data.get('depart') not in geo.airports_cities or data.get('arrive') not in geo.airports_cities):
+            raise exceptions.ParseError('Airport not found for Flight transfer')
+
+    @classmethod
+    def validate_britain(cls, data):
+        """
+        Validate Britain can have only Flight transfer
+        """
+
+        if ((data.get("arrive_country") == 'Britain') ^ (data.get("depart_country") == "Britain")) and \
+                data.get('transfer_type') != "Flight":
+            raise exceptions.ParseError('Britain transfer can only be via airport')
+
+
+    def create(self, validated_data):
+        self.validate_britain(validated_data)
+        self.validate_city(validated_data)
+        self.validate_airport(validated_data)
+
+        return super().create(validated_data)
