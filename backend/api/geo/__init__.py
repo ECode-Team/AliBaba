5d01307ebe6761aa87f1839fa31444cb5e039112
ixor911 <igorbutenko9111@gmail.com>
2025-07-24 13:21:12 +0200

    Geo module

---
+++ backend/api/geo/__init__.py
@@ -0,0 +1,13 @@
+import os
+import yaml
+
+
+with open(f"{os.path.abspath(__file__)}/../geo.yaml", "r", encoding="utf-8") as file:
+    data = yaml.safe_load(file)
+
+geos = data.get('geos')
+airports = data.get('airports')
+
+geos_cities = [item for value in geos.values() for item in value]
+airports_cities = [item for value in airports.values() for item in value]
+
