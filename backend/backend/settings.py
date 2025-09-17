4aa2f2c0985aa6607c76b73348e3e85da514a28b
ixor911 <igorbutenko9111@gmail.com>
2025-09-17 18:59:32 +0200

    settings for hosting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,10 +87,20 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backend112$alibaba1',
+        'HOST': 'backend112.mysql.pythonanywhere-services.com',
+        'USER': 'backend112',
+        'PASSWORD': 'qwerty911'
     }
 }
 
