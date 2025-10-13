df68cb6f18ff76fb96145d5236b85c952c355144
parham0121 <parysolh@gmail.com>
2025-10-13 14:44:36 +0100

    dixed some bug

--- frontend/vite-project/src/Pages/bookingpage/vila.jsx
+++ frontend/vite-project/src/Pages/bookingpage/vila.jsx
@@ -1,5 +1,16 @@
-export const Vila = () => {
-    return ( <div>
+import React from "react";
+import { Search } from "../../Components/mainpage/search";
 
-    </div>)
-}
+const Vila = () => {
+  return (
+    <div className="vila-page-container" style={{ minHeight: "60vh" }}>
+      <Search />
+      <div className="vila-content">
+        <h1>Vila Booking Page</h1>
+        <p>Find and book your perfect villa accommodation.</p>
+      </div>
+    </div>
+  );
+};
+
+export { Vila };
